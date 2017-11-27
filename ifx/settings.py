ce6ac1f723c8776d01b2f16efd4d74507e34b063
TalTzf <tal.tzf@gmail.com>
2017-11-27 20:59:40 +0200

    add Movie simple class model

--- ifx/settings.py
+++ ifx/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions'
+    'django_extensions',
+
+    'movies',
 ]
 
 MIDDLEWARE = [
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    date = models.DateField()
+    length = models.DecimalField(max_digits=12, decimal_places=2)
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
