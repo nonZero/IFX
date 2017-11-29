ab2bc7a58a7dd4301136b80df088e713c338c2cf
TalTzf <tal.tzf@gmail.com>
2017-11-29 19:49:22 +0200

    add list and detail pages with templates

---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = "movies"
+urlpatterns = [
+    url(r"^$", views.home, name='list'),
+    url(r"^(?P<id>[0-9]+)/$", views.detail, name='detail'),
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from movies.models import Movie
+
+
+def home(request):
+    qs = Movie.objects.order_by('-date')
+    d = {
+        'objects': qs,
+    }
+    return render(request, "movies/home.html", d)
+
+
+def detail(request, id):
+    o = Movie.objects.get(id=id)
+    d = {
+        'object': o,
+    }
+    return render(request, "movies/detail.html", d)
