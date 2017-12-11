19733bbb5573dd95ac7dec16fdfdcd55ef3077e3
TalTzf <tal.tzf@gmail.com>
2017-12-11 13:55:55 +0200

    Extend Movie and add Tag models

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,15 @@ class Movie(models.Model):
     length = models.DecimalField(max_digits=12, decimal_places=2)
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
+    bid = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    bid = models.IntegerField()
+    title = models.CharField(max_length=300)
+    type1_id = models.CharField(max_length=300)
 
     def __str__(self):
         return self.title
