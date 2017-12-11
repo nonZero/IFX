e3c94fddc224157d23b3a50b18420b5dad3def3a
TalTzf <tal.tzf@gmail.com>
2017-12-11 15:24:26 +0200

    Add import_tags command

---
+++ movies/management/commands/import_tags.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import random
+import silly
+
+from django.core.management.base import BaseCommand
+
+from movies.models import Tag
+
+
+class Command(BaseCommand):
+    help = "Import tags."
+
+    def add_arguments(self, parser):
+        parser.add_argument('f', type=str)
+
+    def handle(self, f, **options):
+        tags = pd.read_csv(f, delimiter='\t')
+
+        for i, row in tags.iterrows():
+            print(row.book_id_s, row.title)
+            o = Tag()
+            o.title = row.title
+            o.bid = row.book_id_s
+            o.save()
