8cb8ee28c7de1278e3d3e6219563a7efc15b84e3
polalucash <polalucash@gmail.com>
2017-12-12 15:03:51 +0200

    added settings for postgress db

--- ifx/settings.py
+++ ifx/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ifx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ifx',
+        'USER': 'ifx',
+        'PASSWORD': 'ifx@DB',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
