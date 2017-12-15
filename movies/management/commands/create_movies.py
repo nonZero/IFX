1a4a33c958f7f932ea1c233989aa7f12e4085a32
TalTzf <tal.tzf@gmail.com>
2017-12-15 14:35:06 +0200

    Fix create_movies after changes in Movie model

--- movies/management/commands/create_movies.py
+++ movies/management/commands/create_movies.py
@@ -1,5 +1,5 @@
 import random
-
+from tqdm import tqdm
 import silly
 from django.core.management.base import BaseCommand
 
@@ -12,13 +12,28 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('n', type=int)
 
+        parser.add_argument(
+            '--readonly',
+            action='store_true',
+            dest='readonly',
+            help='Parse it without saving to database',
+        )
+
     def handle(self, n, **options):
+        last_bid = Movie.objects.latest('bid').bid
+
+        progress = tqdm(total=n)
+
         for i in range(n):
             o = Movie()
+            o.bid = last_bid + i + 1
             o.title = silly.a_thing()
-            o.length = "{}.{}".format(random.randint(1, 100),
-                                      random.randint(0, 99))
-            o.date = silly.datetime().date()
-            o.description = "\n".join(
-                [silly.paragraph(), silly.paragraph(), silly.paragraph()])
-            o.save()
+            o.year = random.randint(1900, 2100)
+            o.lang = random.choice(['Heb', 'Eng', 'Unknown'])
+            o.full_clean()
+
+            if not options['readonly']:
+                o.save()
+            progress.update(1)
+
+        progress.close()
