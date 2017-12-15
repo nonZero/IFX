529d4eb96075571b6bd21ebb5cbd58bbd081441d
TalTzf <tal.tzf@gmail.com>
2017-12-15 19:03:15 +0200

    import movies from json file format

--- movies/management/commands/import_movies.py
+++ movies/management/commands/import_movies.py
@@ -19,9 +19,19 @@ class Command(BaseCommand):
             action='store_true',
             dest='readonly',
             help='Parse it without saving to database',
-    )
+        )
+
+        parser.add_argument(
+            '--json',
+            action = 'store_true',
+            dest = 'json',
+            help = 'input is json file',
+        )
 
     def handle(self, f, **options):
+        if options['json']:
+            return self.handle_json(f, **options)
+
         df = pd.read_csv(f, delimiter='\t')
 
         progress = tqdm(total=len(df))
@@ -35,6 +45,28 @@ class Command(BaseCommand):
             o.title = row.title
             o.year = None if np.isnan(row.year) else row.year
             o.lang = row.lang
+            o.full_clean()
+
+            if not options['readonly']:
+                o.save()
+            progress.update(1)
+
+        progress.close()
+
+    def handle_json(self, f, **options):
+        df = pd.read_json(f)
+
+        progress = tqdm(total=len(df))
+
+        for i, row in df.iterrows():
+            if Movie.objects.filter(bid=row.fields['bid']).exists():
+                continue
+
+            o = Movie()
+            o.bid = row.fields['bid']
+            o.title = row.fields['title']
+            o.year = None if np.isnan(row.fields['year']) else row.fields['year']
+            o.lang = row.fields['lang']
             o.full_clean()
 
             if not options['readonly']:
