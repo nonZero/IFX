7e7910914717bd4f9ae181ec4cc04bcb6d99b786
Anat Knebel Shoval <anat.shoval@gmail.com>
2017-12-28 11:39:49 +0200

    set settings urls local style and homePage for lang and set menu-bar

--- ifx/settings.py
+++ ifx/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -111,7 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    ('en-us', _('English')),
+    ('he', _('Hebrew'))
+]
+
+#LANGUAGE_CODE = (
+#    ('en-us', 'English'),
+#    ('he', 'Hebrew')
+#)
 
 TIME_ZONE = 'UTC'
 
