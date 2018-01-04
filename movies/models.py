9d24b6e7f137da44ada22384539352ba7b48c7f5
polalucash <polalucash@gmail.com>
2018-01-04 08:22:15 +0200

    removed type1 id from tag model

--- movies/models.py
+++ movies/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Tag(models.Model):
     tid = models.IntegerField(unique=True)
     title = models.CharField(max_length=300)
-    type1_id = models.CharField(max_length=300, blank=True)
     
     def __str__(self):
         return self.title
