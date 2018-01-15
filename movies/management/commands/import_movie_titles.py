e6e9a92dcdbeea19d6e4beec58261af92e50f252
Udi Oron <udioron@gmail.com>
2018-01-15 19:06:43 +0200

    Fixed import scripts

--- movies/management/commands/import_movie_titles.py
+++ movies/management/commands/import_movie_titles.py
@@ -1,38 +1,48 @@
+from collections import Counter
+from pprint import pprint
+
 import pandas as pd
+from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
-from django.core.management.base import BaseCommand
-
-from movies.models import Tag, Field, Tag_Field, models, Movie, Movie_Title
+from movies.models import models, Movie
 
 
 class Command(BaseCommand):
-	help = "Import movie titles translations."
-	
-	def add_arguments(self, parser):
-		parser.add_argument('f', type=str,
-		                    help='file path to import from')
-		
-		parser.add_argument(
-			'--readonly',
-			action='store_true',
-			dest='readonly',
-			help='Parse it without saving to database',
-		)
-	
-	def handle(self, f, **options):
-		df = pd.read_csv(f, delimiter='\t')
-		progress = tqdm(total=len(df))
-		for i, row in df.iterrows():
-			try:
-				if str(row.book_id).isdigit() and row.lang_id in ["HEB", "ENG"]:
-					m = Movie.objects.get(bid=row.book_id)
-					mt = Movie_Title(movie=m, title=row.title, lang=row.lang_id )
-					if not options['readonly']:
-						mt.save()
-			except models.ObjectDoesNotExist:
-				# log error to server?
-				print("\nMovie was not found in DB, line=" + str(i))
-			finally:
-				progress.update(1)
-		progress.close()
+    help = "Import movie titles translations."
+
+    def add_arguments(self, parser):
+        parser.add_argument('f', type=str,
+                            help='file path to import from')
+
+        parser.add_argument(
+            '--readonly',
+            action='store_true',
+            dest='readonly',
+            help='Parse it without saving to database',
+        )
+
+    def handle(self, f, **options):
+        c = Counter()
+        try:
+            df = pd.read_csv(f, delimiter='\t')
+            progress = tqdm(total=len(df))
+            for i, row in df.iterrows():
+                try:
+                    if str(row.book_id).isdigit():
+                        m = Movie.objects.get(bid=row.book_id)
+                        if row.lang_id == "ENG":
+                            m.title_en = row.title
+                        elif row.lang_id == "HEB":
+                            m.title_he = row.title
+                        else:
+                            assert False, row
+                        c[row.lang_id] += 1
+                except models.ObjectDoesNotExist:
+                    # log error to server?
+                    c['missing'] += 1
+                finally:
+                    progress.update(1)
+            progress.close()
+        finally:
+            pprint(c.most_common())
