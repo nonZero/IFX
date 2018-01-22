e30b49c83095fbabab944aa45aa7ae67080126f9
polalucash <polalucash@gmail.com>
2018-01-23 00:02:26 +0200

    Movie_Tag_Field relationship import bug fix

--- movies/management/commands/import_relationship.py
+++ movies/management/commands/import_relationship.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from django.core.exceptions import ObjectDoesNotExist
 from tqdm import tqdm
 
 from django.core.management.base import BaseCommand
@@ -22,13 +23,22 @@ class Command(BaseCommand):
 
     def handle(self, f, **options):
         df = pd.read_csv(f, delimiter='\t')
-
+        
+        mrp_counter = 0
+        mtf_counter = 0
+        bad_relationship_count = 0
+        fields_removed = 0
+        roles_added = 0
+        count_total = 0
+        count_movie_missing = 0
+        
         progress = tqdm(total=len(df))
 
         for i, row in df.iterrows():
+            count_total += 1
             try:
                 m = Movie.objects.get(bid=row.book_id)
-                if(Person.objects.exists()):
+                try:
                     # Movie-Person-Role
                     p = Person.objects.get(tid=row.book2_id[1:])
                     r, created = Role.objects.get_or_create(tid=row.lif)
@@ -37,19 +47,36 @@ class Command(BaseCommand):
                         r.title_he = f.title
                         if not options['readonly']:
                             r.save()
-                        f = Field.objects.get(fid=row.lif).delete()
-                    mpr = Movie_Role_Person(movie=m, person=p, role=r)
+                            roles_added+=1
+                        f.delete()
+                        fields_removed+=1
+                    mrp = Movie_Role_Person(movie=m, person=p, role=r)
                     if not options['readonly']:
-                        mpr.save()
-                else:
-                    t = Tag.objects.get(tid=row.book2_id[1:])
-                    mtf = Movie_Tag_Field(movie=m, tag = t, field = f)
-                    if not options['readonly']:
-                        mtf.save()
+                        mrp.save()
+                        mrp_counter+=1
+                except:
+                    try:
+                        f = Field.objects.get(fid=row.lif)
+                        t = Tag.objects.get(tid=row.book2_id[1:])
+                        mtf = Movie_Tag_Field(movie=m, tag = t, field = f)
+                        if not options['readonly']:
+                            mtf.save()
+                            mtf_counter+=1
+                    except models.ObjectDoesNotExist:
+                        bad_relationship_count+=1
             except models.ObjectDoesNotExist:
                 # log error to server?
-                print("Movie, Tag or Field was not found in DB, line=" + str(i))
+                print("Movie was not found in DB, line=" + str(i))
+                count_movie_missing+=1
             finally:
                 progress.update(1)
-
         progress.close()
+
+        print('Report:')
+        print('Total rows={}'.format(count_total))
+        print('Movie_Role_Person found={}'.format(mrp_counter))
+        print('Movie_Tag_Field found={}'.format(mtf_counter))
+        print('Movies missing={}'.format(count_movie_missing))
+        print('bad relationship count={}'.format(bad_relationship_count))
+        print('fields removed ={}'.format(fields_removed))
+        print('roles added ={}'.format(roles_added))
