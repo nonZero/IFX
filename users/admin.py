fcc12be9786641c8bb4708efb942ccd56c45fbc9
Udi Oron <udioron@gmail.com>
2018-02-23 10:24:27 +0200

    User admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,30 @@
-from authtools.admin import UserAdmin
+import authtools.admin
 from django.contrib import admin
 
 from . import models
 
-admin.register(models.User, UserAdmin)
+BASE_FIELDS = (None, {
+    'fields': (
+        'email',
+        'name_he',
+        'name_en',
+        'full_name_he',
+        'full_name_en',
+        'bio_he',
+        'bio_en',
+        'is_team_member',
+        'is_data_volunteer',
+        'password'
+    )
+})
+
+
+class UserAdmin(authtools.admin.UserAdmin):
+    fieldsets = (
+        BASE_FIELDS,
+        authtools.admin.ADVANCED_PERMISSION_FIELDS,
+        authtools.admin.DATE_FIELDS,
+    )
+
+
+admin.site.register(models.User, UserAdmin)
